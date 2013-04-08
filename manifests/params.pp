09aeb62d05bde703f304a6928e96a9ecc697a89c
Lars Tobias Skjong-Børsting <larstobi@relatime.no>
2013-04-08 12:51:17 +0200

    Specify r10k_bin for Ubuntu LTS lucid and precise.

--- manifests/params.pp
+++ manifests/params.pp
@@ -5,8 +5,11 @@ class r10k::params {
     'squeeze': {
       $r10k_bin = '/var/lib/gems/1.8/bin/r10k'
     }
-    'wheezy': {
+    'precise', 'wheezy': {
       $r10k_bin = '/usr/local/bin/r10k'
+    }
+    'lucid': {
+      $r10k_bin = '/var/lib/gems/1.8/bin/r10k'
     }
   }
 }
